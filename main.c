31fb8c6c9b61b1d4c0897cc3cb35efd01dcf9235
Charles Anderson <charles.anderson@uon.edu.au>
2019-04-02 01:10:47 +0000

    started

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int Dec (int x);
+
+int main()
+{
+    int x;
+    scanf("Please enter your message %d\n", &x);
+    printf("You entered %d\n", x);
+}
+int Dec (int x) 
+{
+
+    return 0;
+}
