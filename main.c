43728c368b029c59e499fe20286cb7c2d4279a14
Charles Anderson <charles.anderson@uon.edu.au>
2019-04-02 02:53:43 +0000

    Created primitive decrypt code

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-int Dec (int x);
-
 int main()
 {
-    int x;
-    scanf("Please enter your message %d\n", &x);
-    printf("You entered %d\n", x);
+    
+    char message[100], ch;
+    int i, k = 7;
+    
+    printf("Please enter your message:\n"); 
+    scanf("%s", message); 
+    printf("%s\n", message);
+    for (i = 0; message[i] != '\0'; ++i) {
+        ch = message[i];
+        
+        if ((ch >= 'a') && (ch <= 'z')){ 
+            ch = ch + k;
+// lower case
+            message[i] = ch;
+        }
+        else if ((ch >= 'A') && (ch <= 'Z')){
+            ch = ch + k;
+            message[i] = ch;
+// UPPER CASE. But how to get punctuations and " " in? hmm?
+        }
 }
-int Dec (int x) 
-{
 
-    return 0;
+    printf("Encrypted message: %s\n", message);
 }
