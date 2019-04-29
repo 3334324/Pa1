fa33d8c8dd1147af93e07b1cd5c7b9da9d02c978
3334324 <49174578+3334324@users.noreply.github.com>
2019-04-29 17:24:09 +1000

    Add files via upload
    final

--- main.c
+++ main.c
@@ -1,90 +1,86 @@
 #include <stdio.h>
 
 /*char e(char message);*/
-int main()
-{   
+int main() 
+{
     /*char cipher;
     printf("Enter 1 for Rotation Cipher\nEnter 2 for Substitution Cipher\n");
     scanf("%c", &cipher);
     Didnt get around to substitution whoops*/
     
-    char crypt;
-    int k = 25;// k is key and is how many places each letter is disp
+    char crypt, message[100], ch;
+    int i, k = 25;// k is key (variable) and is how many places each letter is displaced
     printf("Enter 1 for Encryption\nEnter 2 for Decryption\n");
     scanf("%c", &crypt);
     
-    
-    
-    char message[100], ch;
-    int i;
-    
-    if(crypt = 2) {
+    if(crypt = 2) 
+    {
         printf("Please enter your message:\n"); 
         scanf("%s", message); 
         for (i = 0; message[i] != '\0'; ++i) // message cannot be 'nothing'
-{
+        {
             ch = message[i];
     
             if ((ch >= 'a') && (ch <= 'z'))
-{ 
+            { 
                 ch = ch + k;
                 if (ch > 'z') // This bit helps when the letter + key overflows
-{
+                {
 				    ch = ch - 'z' + 'a' - 1;
-}// lower case
+            }// lower case
             message[i] = ch;
-}
+        }
         if ((ch >= 'A') && (ch <= 'Z'))
-{
+            {
             ch = ch + k;
             if(ch > 'Z')
-{
+                {
 		        ch = ch - 'Z' + 'A' - 1;
-}           message[i] = ch;
+            }       
+                message[i] = ch;
 // UPPER CASE.
         
         
-}
-}
+        }
+    }
 
     printf("Encrypted message: %s\n", message);
+    
 }
-} 
     else 
-{
+    {
         printf("Please enter your encrypted message:\n"); 
         scanf("%s", message); 
         for (i = 0; message[i] != '\0'; ++i) // message cannot be 'nothing'
-{
+        {
             ch = message[i];
     
             if ((ch >= 'a') && (ch <= 'z'))
-{ 
+            { 
                 ch = ch - k;
                 if (ch < 'a') // This bit helps when the letter + key overflows
-{
+                {
 				    ch = ch + 'z' - 'a' + 1;
-}// lower case
+            }// lower case
             message[i] = ch;
-}
+        }
         if ((ch >= 'A') && (ch <= 'Z'))
-{
+            { 
             ch = ch - k;
             if(ch < 'A')
-{
+                {
 		    	ch = ch + 'Z' - 'A' + 1;
-}           message[i] = ch;
+            }   message[i] = ch;
 // UPPER CASE.
         
         
-}
-}
+        }
+    }
 
     printf("Decrypted message: %s\n", message);
 }
-
- 
-    
+    return 0;
+}
 
  /* char e(char message)
 {
